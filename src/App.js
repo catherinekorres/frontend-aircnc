ca8bb7b42541a8864ae58f9f7f7c6f91b8385e18
Catherine <cathy.korres@gmail.com>
2019-10-02 13:55:38 -0300

    create initial page content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
   return (
-    <div className="App">
+    <div className="container">
+      <img src={logo} alt="AirCnC"/>
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+        </p>
+
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input 
+            id="email"
+            type="email"
+            placeholder="Seu melhor e-mail"
+          />
+
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
